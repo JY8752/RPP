a9a9b3fe191cd55515f1772620933a6228a9a019
JY8752 <j.y.8752@gmail.com>
2021-02-23 10:10:09 +0900

    コミット漏れ

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :roles
 
     validates :name, presence: true
     validates :password, presence: true, length: { in: 6..20 }
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_100620) do
+ActiveRecord::Schema.define(version: 2021_02_23_005300) do
+
+  create_table "roles", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "role", null: false
+    t.integer "level", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_roles_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
