99ac2bd2058af028d87775226b8db6ec0f701c2a
JY8752 <j.y.8752@gmail.com>
2021-02-23 11:43:16 +0900

    first commit

--- back/app/controllers/sessions_controller.rb
+++ back/app/controllers/sessions_controller.rb
@@ -3,18 +3,22 @@ class SessionsController < ApplicationController
 
   # 認証
   def sign_in
-    user = User.find_by(name: session_params[:name]).authenticate(params[:password])
-    session[:user_id] = user.id
-    render json: { message: "sign in success" }
+    user = User.find(session_params[:user_id]).authenticate(params[:password])
+    if user
+        session[:user_id] = user.id
+        render json: { message: "sign in success" }
+    else 
+        render json: { message: "sign in fail"}
+    end
   end
 
   def sign_out
-    session[:user_id] = nil
+    session.delete(:user_id)
   end
 
   private
 
   def session_params
-    params.require(:session).permit(:name, :password)
+    params.require(:session).permit(:user_id, :password)
   end
 end
