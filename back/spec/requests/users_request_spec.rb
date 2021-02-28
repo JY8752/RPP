fab4f8425985f59f2c45852cfc1013261c103f87
JY8752 <j.y.8752@gmail.com>
2021-02-28 11:22:46 +0900

    first commit

---
+++ back/spec/requests/users_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+    # ユーザー一覧取得
+    describe 'GET /api/v1/users' do
+
+    end
+    #ユーザー取得
+    describe 'Get /api/v1/users/{ user_id }' do
+
+    end
+    # ユーザー作成
+    describe 'POST /api/v1/users' do
+
+    end
+    # ユーザー更新
+    describe 'PUT /api/v1/users/{ user_id ' do
+
+    end
+    # ユーザー削除
+    describe 'DELETE /api/users/{ user_id' do
+        
+    end
+end
